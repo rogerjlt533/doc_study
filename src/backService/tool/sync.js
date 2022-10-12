8230a2792856da02d40247f2cbb3b91e93d9f662
fallen <rogerk08@126.com>
2022-10-12 19:55:45 +0800

    * 标签置顶状态修改

--- src/backService/tool/sync.js
+++ src/backService/tool/sync.js
@@ -302,3 +302,35 @@ exports.delete = async function (id) {
 exports.deleteBatch = async function (where) {
     return await sqlite.delete('syncs', where)
 }
+
+/**
+ * tag top 队列
+ * @param user_id
+ * @param tag_id
+ * @returns {Promise<*>}
+ */
+exports.getUploadTagTop = async function (user_id, tag_id) {
+    const sql = 'select * from syncs where user_id=? and sync_type=31 and sync_direct=2 and tag_id=? order by status desc'
+    return await sqlite.get(sql, [user_id, tag_id])
+}
+
+/**
+ * tag top 队列
+ * @param user_id
+ * @param tag_id
+ * @returns {Promise<*>}
+ */
+exports.getUploadTagTop = async function (user_id, tag_id) {
+    const sql = 'select * from syncs where user_id=? and sync_type=31 and sync_direct=2 and tag_id=? order by status desc'
+    return await sqlite.get(sql, [user_id, tag_id])
+}
+
+/**
+ *
+ * @param user_id
+ * @returns {Promise<void>}
+ */
+exports.uploadTagTopList = async function (user_id) {
+    const sql = 'select * from syncs where user_id=? and sync_type=31 and sync_direct=2 order by status desc'
+    return await sqlite.all(sql, [user_id, tag_id])
+}
